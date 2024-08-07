9f51f9aa5a94ae870bdc93cbc66f7ee4d3877752
moguDev <mail@sho-horiguchi.jp>
2024-08-08 00:25:24 +0900

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 if [ "$RAILS_ENV" = "production" ]; then
-bundle exec rails assets:clobber
+bundle install
 bundle exec rails assets:precompile
+bundle exec rails assets:clean
 bundle exec rails db:migrate
 fi
 
