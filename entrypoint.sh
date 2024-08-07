e7b2471fe858fda6553f7a2bc8fdd349a5c30c15
moguDev <mail@sho-horiguchi.jp>
2024-08-08 00:11:40 +0900

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-if [ "$RAILS_ENV" = "production" ]; then
 bundle exec rails assets:clobber
 bundle exec rails assets:precompile
 bundle exec rails db:migrate
-fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
