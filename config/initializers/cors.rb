5666513424fa676fbedb99fc12200b934397d81f
moguDev <mail@sho-horiguchi.jp>
2024-08-08 11:54:46 +0900

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,12 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3030", "https://buckets-flow.vercel.app/"
+    origins "http://localhost:3030", "https://buckets-flow.vercel.app"
 
     resource "*",
       headers: :any,
       expose: ["access-token", "expiry", "token-type", "uid", "client"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true # クッキーの送信を許可
+      credentials: true
   end
 end
