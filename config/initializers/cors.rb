b4f758c6061178aa6a0c876c5cc36882bf033332
moguDev <mail@sho-horiguchi.jp>
2024-08-10 11:45:37 +0900

    edit cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3030","https://buckets-flow.com","https://www.buckets-flow.com", "https://buckets-flow.vercel.app"
+    origins "http://localhost:3030","https://buckets-flow.com","https://www.buckets-flow.com"
 
     resource "*",
       headers: :any,
